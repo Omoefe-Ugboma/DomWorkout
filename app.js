ecbe7eac15dd54b4352771a5c692f1b0d4baf9b6
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-09-13 10:39:08 +0100

    Creating the alert method

--- app.js
+++ app.js
@@ -14,7 +14,15 @@ UI.prototype.addBookList = function (book) {
 
     // create tr element
     const row = document.createElement('tr');
+    // Insert cols
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete">X</a></td>
+    `;
 
+    list.appendChild(row)
 }
 
 // Event Listeners
@@ -31,6 +39,14 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
 
     // Instance of UI
     const ui = new UI();
+
+    // Validate
+    if (title === '' || author === '' || isbn === '') {
+        // Error
+        ui.showAlert('Please fill in all fields', 'error');
+    } else {
+
+    }
 
     console.log(ui);
 
