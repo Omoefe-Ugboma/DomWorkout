014191c4fad778b2eae86a0be4b0135c18054639
jkomoefey23e@yahoo.co.uk <jkomoefey23e@yahoo.co.uk>
2022-09-15 20:04:12 +0100

    update on class

---
+++ appes6.js
@@ -0,0 +1,23 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    addBookToList(book) {
+        const list = document.getElementById('book-list');
+
+    }
+    showAlert(message, className) {
+
+    }
+    deleteBook(target) {
+
+    }
+    clearFiel() {
+
+    }
+}
